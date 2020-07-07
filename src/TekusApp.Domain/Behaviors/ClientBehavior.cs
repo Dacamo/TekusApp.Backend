e5a40b8c1215b0dbe7cc54eb0898b9ed429ad55d
Dacamo <castrom1998@gmail.com>
2020-07-07 15:52:29 -0500

    Adicion de interfaz e Implementación de IClientBehavior

---
+++ src/TekusApp.Domain/Behaviors/ClientBehavior.cs
@@ -0,0 +1,49 @@
+﻿using FamiliesApp.Domain.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TekusApp.Domain.Models;
+
+namespace TekusApp.Domain.Behaviors
+{
+    public class ClientBehavior : IClientBehavior
+    {
+        private readonly IDataStorage<Client> _clientRepository;
+
+        public ClientBehavior(IDataStorage<Client> clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        public async Task CreateAsync(Client client)
+        {
+            if(client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+
+            await _clientRepository.InsertAsync(client);
+        }
+
+        public async Task DeleteAsync(Client client)
+        {
+            await _clientRepository.DeleteAsync(client);
+        }
+
+        public async Task<List<Client>> GetAllAsync()
+        {
+            return await _clientRepository.FindAllAsync();
+        }
+
+        public async Task<Client> GetByIdAsync(int id)
+        {
+            return await _clientRepository.FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task UpdateAsync(Client client)
+        {
+            await _clientRepository.UpdateAsync(client);
+        }
+    }
+}
---
+++ src/TekusApp.Domain/Behaviors/IClientBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TekusApp.Domain.Models;
+
+namespace TekusApp.Domain.Behaviors
+{
+    public interface IClientBehavior
+    {
+        Task CreateAsync(Client client);
+        Task<List<Client>> GetAllAsync();
+        Task<Client> GetByIdAsync(int id);
+        Task DeleteAsync(Client client);
+        Task UpdateAsync(Client client);
+    }
+}
