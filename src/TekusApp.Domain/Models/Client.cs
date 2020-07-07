5860abcfac85489de94ea5268952850e0ec5f882
Dacamo <castrom1998@gmail.com>
2020-07-07 13:15:34 -0500

    Se agregan los modelos base

---
+++ src/TekusApp.Domain/Models/Client.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TekusApp.Domain.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string NIT { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
