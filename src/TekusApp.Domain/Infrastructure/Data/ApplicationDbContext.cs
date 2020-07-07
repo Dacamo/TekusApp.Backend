bee5ef07462816edcb11cd6354d8d9fb3d1da97d
Dacamo <castrom1998@gmail.com>
2020-07-07 13:14:50 -0500

    Se crea el DbContext y se configura el Startup

---
+++ src/TekusApp.Domain/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TekusApp.Domain.Models;
+
+namespace TekusApp.Domain.Infrastructure.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Service> Services { get; set; }
+    }
+}
--- src/TekusApp/Startup.cs
+++ src/TekusApp/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using TekusApp.Domain.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace TekusApp
 {
@@ -26,6 +21,11 @@ namespace TekusApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                serverOptions => serverOptions.MigrationsAssembly("TekusApp"))
+            );
+
             services.AddControllers();
 
             // Register the Swagger generator, defining 1 or more Swagger documents
