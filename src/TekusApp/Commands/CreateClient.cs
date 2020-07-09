752c113152c4e0ad391c67b3d1f237bc7e1912bd
Dacamo <castrom1998@gmail.com>
2020-07-09 13:45:40 -0500

    Modificación del command con los campos requeridos

--- src/TekusApp/Commands/CreateClient.cs
+++ src/TekusApp/Commands/CreateClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace TekusApp.Commands
 {
     public class CreateClient
     {
+        [Required]
         public string NIT { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
     }
 }
